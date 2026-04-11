fc6a5fdd0a6da209f2683da8645d4529c7cad1bf
abubakkarsiddik123 <abu089604@gmail.com>
2026-04-12 00:18:41 +0600

    project successfull

--- src/Components/navbar/NavBar.jsx
+++ src/Components/navbar/NavBar.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 import dollerImg from "../../assets/Coin.png"
+import logoImg from '../../assets/logo.png'
 
 const NavBar = ({coin}) => {
   return (
-    <div className="navbar bg-base-100 shadow-sm container mx-auto">
+    <div className="navbar bg-base-100 shadow-sm container mx-auto mb-4 mt-4 rounded-xl">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        {/* <a className="btn btn-ghost text-xl"></a> */}
+        <img src={logoImg} alt="" />
       </div>
-      <div className="flex-none">
-        <button className="btn flex justify-between items-center gap-2 font-bold text-xl">
+     
+      <div className="flex justify-center items-center gap-4">
+      <div className="">
+        <ul className="flex items-center gap-4">
+          <li><a href="/">Home</a></li>
+          <li><a href="/Fixture">Fixture</a></li>
+          <li><a href="/Teams">Teams</a></li>
+          <li><a href="/Schedules">Schedules</a></li>
+        </ul>
+      </div>
+       <div className="">
+         <button className="btn flex justify-between items-center gap-2 font-bold text-xl">
         {coin} Coins
           <img src={dollerImg} alt="" />
         </button>
+       </div>
       </div>
     </div>
   );
